eada11e53221613ec39a756e45ee2e282abf8649
Heng Li <lh3@me.com>
2020-04-19 15:50:23 -0400

    print positional arguments in test

--- test/testArg.nim
+++ test/testArg.nim
@@ -1,5 +1,10 @@
 import klib
 
 var argv = getArgv()
+echo "Options:"
 for opt, arg in getopt(argv, "x:y", @["foo=", "bar"]):
-  echo "opt: \"", opt, "\"; arg: \"", arg, "\""
+  echo "  opt: \"", opt, "\"; arg: \"", arg, "\""
+
+echo "Arguments:"
+for x in argv:
+  echo "  ", x
