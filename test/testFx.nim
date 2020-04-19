9d4c33bbecf0223d10e484556ffc4e7c3fbecb64
Heng Li <lh3@me.com>
2020-04-19 15:47:31 -0400

    added Fastx test cases

---
+++ test/testFx.nim
@@ -0,0 +1,13 @@
+import klib
+
+var
+  argv = getArgv()
+  f = xopen[GzFile](argv[0])
+  r = FastxRecord()
+while f.readFastx(r):
+  echo "name: ", r.name
+  echo "comment: ", r.comment
+  echo "seq: ", r.seq
+  echo "qual: ", r.qual
+echo r.status
+f.close()
